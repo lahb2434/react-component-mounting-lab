230405a0c9466df48fe4b4dcb254faad0a039b25
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-11-22 15:17:21 -0500

    commit tree 4832883a3526ff10d989cb7a80db99351607b888

--- src/Timer.js
+++ src/Timer.js
@@ -8,15 +8,27 @@ class Timer extends Component {
 
   // add your code here
 
+  componentDidMount() {
+    this.interval = setInterval(this.clockTick, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+  
   render() {
     const { time, color } = this.state;
-    return (
-      <section className="Timer" style={{ background: color }}>
+    return (<div>
+      <div className="Timer" style={{ background: color }}>
         <h1>{time}</h1>
-        <button onClick={this.stopClock}>Stop</button>
+        
         <aside className="mountText">Mounted</aside>
         <small onClick={this.handleClose}>X</small>
-      </section>
+        <div className="Timer" style={{ background: color }}><button onClick={this.stopClock}>Stop</button></div>
+
+      </div>
+
+              </div>
     );
   }
 
